3e7f4cfc647a528ad6e37036ba132cb1cfcf1a43
Ivan mendez <mendezivan604@gmail.com>
2026-02-28 16:23:32 -0600

    Ejercicio 2

--- Ejercicio_1/Program.cs
+++ Ejercicio_1/Program.cs
@@ -9,6 +9,15 @@ for (int i = 1; i <= 5; i++)
     Console.WriteLine();
 }
 
-
+//Ejercicio 2
+Console.WriteLine("Ejercicio 2");
+for (int i = 1; i <= 4; i++)
+{
+    for (int j = 1; j <= 6; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
 
 
